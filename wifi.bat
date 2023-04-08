bd9a217c0d105136bb20d30fd390e8a89c4073e7
Glenn <8988140+GlennChon@users.noreply.github.com>
2023-04-08 12:35:38 -0400

    Create wifi.bat
    Displays connected wlan profile name and password if security key is present.

---
+++ wifi.bat
@@ -0,0 +1,22 @@
+@ECHO off
+SETLOCAL enabledelayedexpansion
+
+SET profile_name=
+SET available=
+SET wifi_pass=
+
+FOR /F "tokens=2 delims=: " %%n in ('netsh wlan show interfaces ^| find "Profile"') DO (
+	SET "profile_name=%%n"
+)
+FOR /F "tokens=3 delims=: " %%k in ('netsh wlan show profile name^="%profile_name%" ^| findstr "Security key"') DO (
+	SET "available=%%k"
+)	
+FOR /F "tokens=3 delims=: " %%p in ('netsh wlan show profile name^="%profile_name%" key^=clear ^| findstr "Key Content"') DO (
+		SET "wifi_pass=%%p"
+	)	
+	
+ECHO Profile: %profile_name%
+ECHO Security Key: %available%
+IF Present == %available% (
+	ECHO Password: %wifi_pass%
+)
